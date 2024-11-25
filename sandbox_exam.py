115ed0ae3525fa06cbfb43f2f92692bc0591c4f9
Álvaro García Sánchez <alvaroelectronico@gmail.com>
2024-11-25 06:48:04 +0100

    New pyomo_model.py script to get reduced costs, shadow prices and sensitivity analysis.

--- sandbox_exam.py
+++ sandbox_exam.py
@@ -1,29 +1,36 @@
-from exams_pl.problem_class import *
+from old_code.old_problem_class import *
+from pyperclip import *
+
 # from problem_class import *
 
-objective = ('maximize', '2x_1 + 1x_2')
-constraints = ['3x_1 + 1x_2  <= 30',
-               '-4x_1 +3x_2 <= 16',
-               '1x_1 -2x_2 >= 4',
-               '1x_1 + 3x_2 >= 5']
+objective = ('maximize', '- 12x_1 - 2x_2 + 1x_3')
+constraints = ['3x_1 + 2x_2 + 1x_3 >= 3',
+               '4x_1 + 1x_2 - 1x_3 >= 4']
 
-problem = LP_Problem(num_vars=2, objective_function=objective, constraints=constraints)
+problem = LP_Model(num_vars=3, objective_function=objective, constraints=constraints)
 print(problem.formulation_phase1_tex())
 
-print(problem.compose_tableau(problem.simplex_bases))
-print(problem.tableau_basic_sol_tex(problem.simplex_bases[0]))
+copy(problem.compose_tableau(problem.simplex_bases, with_artificials=True))
 
-problem.simplex_bases
-problem.gen_all_bases_info()
-problem.bases_info.keys()
-problem.bases_info[2, 3, 6, 7].pB
-
-array_to_fraction(problem.)
-
-print(matrix_to_tex(array_to_fraction(problem.c2)))
-
-
-
-
-
-basic_solution_to_fraction(problem.bases_info[2, 3, 6, 7])
+copy(problem.compose_tableau(problem.simplex_bases, with_artificials=False))
+copy(problem.compose_tableau(problem.simplex_bases, with_artificials=False))
+#
+#
+# print(problem.compose_tableau([problem.solving_bases[0]]))
+#
+#
+#
+# problem.solving_bases
+# problem.gen_all_bases_info()
+# problem.bases_info.keys()
+# problem.bases_info[2, 3, 6, 7].pB
+#
+# # array_to_fraction(problem.)
+#
+# print(matrix_to_tex(array_to_fraction(problem.c2)))
+#
+#
+#
+#
+#
+# basic_solution_to_fraction(problem.bases_info[2, 3, 6, 7])
